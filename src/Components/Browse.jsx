acbea65c218a438c31c5a9d68c13902aa3859dd8
Jithin2k <jithinjmadhav@gmail.com>
2025-07-23 20:57:45 +0530

    Created API Tokens for API Calls

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Header from './Header'
+import React, { useEffect } from "react";
+import Header from "./Header";
+import { API_OPTIONS } from "../Utils/constant";
 
 const Browse = () => {
+  const getNowPLayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+    
+  };
+
+  useEffect(() => {
+    getNowPLayingMovies();
+  },[])
+
   return (
     <div>
-      <Header/>
+      <Header />
       <h1>This is the browse page</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Browse
+export default Browse;
