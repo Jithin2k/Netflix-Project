bc483eb8b2e882465a80668ffb6700517c6db046
Jithin2k <jithinjmadhav@gmail.com>
2025-07-24 22:09:21 +0530

    Created custom hooks for all movie categories

--- src/Utils/movieSlice.js
+++ src/Utils/movieSlice.js
@@ -4,23 +4,37 @@ const movieSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
-    popularMovies:null,
-    trailerVideo : null,
-
+    popularMovies: null,
+    topRatedMovies: null,
+    upcomingMovies:null,
+    trailerVideo: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
-     addPopularMovies: (state, action) => {
+    addPopularMovies: (state, action) => {
       state.popularMovies = action.payload;
     },
-    addTrailerVideo: (state,action) =>{
+     addUpcomingMovies: (state, action) => {
+      state.upcomingMovies = action.payload;
+    },
+
+    addTopRatedMovies: (state, action) => {
+      state.topRatedMovies = action.payload;
+    },
+    addTrailerVideo: (state, action) => {
       state.trailerVideo = action.payload;
-    }
+    },
   },
 });
 
-export const { addNowPlayingMovies ,addTrailerVideo,addPopularMovies } = movieSlice.actions;
+export const {
+  addNowPlayingMovies,
+  addTrailerVideo,
+  addPopularMovies,
+  addTopRatedMovies,
+  addUpcomingMovies
+} = movieSlice.actions;
 
 export default movieSlice.reducer;
